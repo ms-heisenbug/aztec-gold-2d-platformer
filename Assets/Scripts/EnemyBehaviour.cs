7bdc5d9d8c2c81e2571241a4db5b0aa9bfc950a4
ms-heisenbug <j.piorkowska94@gmail.com>
2020-10-26 22:08:27 +0100

    added enemy patrol

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    float rangeOfSight;
+    float moveSpeed;
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rangeOfSight = 3.5f;
+        animator = GetComponent<Animator>();
+        moveSpeed = 1f;
+    }
+
+    void FixedUpdate()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(distanceToPlayer < rangeOfSight)
+        {
+            Chase();
+        }
+        else
+        {
+            StopChase();
+        }
+    }
+
+    private void Chase()
+    {
+        Debug.Log("Chase");
+
+        animator.SetBool("IsMoving", true);
+
+        if(transform.position.x < player.position.x)
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+    }
+
+    private void StopChase()
+    {
+        rb.velocity = Vector2.zero;
+        animator.SetBool("IsMoving", false);
+    }
+}
---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float moveSpeed = 2f;
+
+    [SerializeField] Transform castPos;
+    float baseCastDistance = 0.3f;
+    bool facingRight;
+    Vector3 baseScale;
+
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        facingRight = true;
+        baseScale = transform.localScale;
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float velocityX = moveSpeed;
+
+        if (!facingRight)
+        {
+            velocityX = -moveSpeed;
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(velocityX));
+        rb.velocity = new Vector2(velocityX, 0);
+
+        if (IsHittingWall() || IsNearEdge())
+        {
+            Flip();
+        }
+    }
+
+    bool IsHittingWall()
+    {
+        float castDistance = baseCastDistance;
+
+        if (!facingRight)
+        {
+            castDistance = -baseCastDistance;
+        }
+
+        Vector3 targetPos = castPos.position;
+        targetPos.x += castDistance;
+
+        Debug.DrawLine(castPos.position, targetPos, Color.red);
+
+        return Physics2D.Linecast(castPos.position, targetPos, 1 << LayerMask.NameToLayer("Ground"));
+    }
+
+    bool IsNearEdge()
+    {
+        float castDistance = baseCastDistance;
+
+        Vector3 targetPos = castPos.position;
+        targetPos.y -= castDistance;
+
+        Debug.DrawLine(castPos.position, targetPos, Color.yellow);
+
+        return !Physics2D.Linecast(castPos.position, targetPos, 1 << LayerMask.NameToLayer("Ground"));
+    }
+
+    void Flip()
+    {
+        Vector3 newScale = baseScale;
+
+        if (facingRight)
+        {
+            newScale.x = -baseScale.x;
+        }
+
+        transform.localScale = newScale;
+
+        facingRight = !facingRight;
+    }
+}
