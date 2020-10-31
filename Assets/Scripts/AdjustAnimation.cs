81588fc56c30016f59ca14f014c1d57e93b1f209
ms-heisenbug <j.piorkowska94@gmail.com>
2020-10-31 16:01:27 +0100

    main menu added

---
+++ Assets/Scripts/AdjustAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustAnimation : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] float animSpeed;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        animator.speed = animSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
