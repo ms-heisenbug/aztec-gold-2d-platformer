19adc1cf08cf4c7d778dab74560f6feef4eaa273
ms-heisenbug <j.piorkowska94@gmail.com>
2020-11-01 16:04:48 +0100

    added orb collecting with totem replacement

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] int numberOfTotems;
+    int orbsNeeded;
+    int orbsFound;
+    [SerializeField] Sprite[] totemSprites;
+    GameObject totemImgs;
+
+    private void Awake()
+    {
+        orbsNeeded = 2 * numberOfTotems;
+        orbsFound = 0;
+        totemImgs = GetTotemImgsGameObj();
+    }
+
+    private GameObject GetTotemImgsGameObj()
+    {
+        GameObject canvasGo = GameObject.Find("UICanvas");
+
+        if(canvasGo != null)
+        {
+            Debug.Log("Canvas");
+            return GameObject.Find("TotemsImgs");
+        }
+        return null;
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OrbFound()
+    {
+        orbsFound += 1;
+        orbsNeeded -= 1;
+        AddNewTotemImg();
+    }
+
+    void AddNewTotemImg()
+    {
+        Debug.Log("Orbs found = " + orbsFound);
+        totemImgs.GetComponent<Image>().sprite = totemSprites[orbsFound];
+    }
+}
---
+++ Assets/Scripts/OrbPickupManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbPickupManager : MonoBehaviour
+{
+    LevelManager lvlManager;
+
+    private void Awake()
+    {
+        lvlManager = FindObjectOfType<LevelManager>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            lvlManager.OrbFound();
+        }
+    }
+}
